20bfbd2728a852532e9a9066373b3177a73a020a
victor tobi <victortobi157@gmail.com>
2022-08-05 23:27:14 +0100

    ljll

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
-import pyglet
 
-file = pyglet.resource.media("audio\message-ringtone-magic.mp3")
+import wave
+import pyaudio
+
+
+
+def play_audio(filename):
+    chunk = 1024
+    wf = wave.open(filename, 'rb')
+    pa = pyaudio.PyAudio
+    stream = pa.open(
+        format=pa.get_format_from_width(wf.getsampwidth()),
+        channels= wf.getnchannels(),
+        rate= wf.getframerate(),
+        output= True
+    )
+    print('jjjjjjj')
+
+    data_stream = wf.readframes(chunk)
+    while data_stream:
+        stream.write(data_stream)
+        data_stream = wf.readframes(chunk)
+
+    stream.close()
+    pa.terminate()
+
+play_audio('./audio/mixkit-sci-fi-reject-notification-896.wav')
